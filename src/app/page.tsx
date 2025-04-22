473e41f9d36e5dbdeeb16db80c66c7e849251a4c
waleedfadl <wwaleedfadl@gmail.com>
2025-04-22 21:56:01 +0200

    clear start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,22 @@
 "use client";
 import {
-	SignedIn,
-	SignedOut,
-	SignInButton,
-	SignOutButton,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
 } from "@clerk/nextjs";
 
 const HomePage = () => {
-	return (
-		<div className="text-3xl">
-			Home Page
-			<SignedOut>
-				<SignInButton />
-			</SignedOut>
-
-			<SignedIn>
-				<SignOutButton />
-			</SignedIn>
-		</div>
-	);
+  return (
+    <div className="text-3xl">
+      Home Page
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
 };
-
 export default HomePage;
